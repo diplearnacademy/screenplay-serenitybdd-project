fb751e98116383795803a7887cab4cad1385fb77
ingjuanfg <juan.isfg@gmail.com>
2024-08-08 20:11:54 -0500

    Cambios en serenity conf y yml

--- src/test/java/co/dlacademy/userinterfaces/ProductoPage.java
+++ src/test/java/co/dlacademy/userinterfaces/ProductoPage.java
@@ -6,5 +6,5 @@ import org.openqa.selenium.By;
 public class ProductoPage {
 
     public static final Target LABEL_TITULO_PAGINA = Target.the("Titulo  de la pagina de productos")
-            .located(By.xpath("//span[contains(.,'Products')]"));
+            .located(By.xpath("//span[contains(.,'{0}')]"));
 }
